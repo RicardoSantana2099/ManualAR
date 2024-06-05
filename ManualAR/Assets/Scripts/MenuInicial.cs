daac4499a88499c696d653da00c6a2134e9a8bf7
PACHECO 2099 <122486806+RicardoSantana2099@users.noreply.github.com>
2024-06-04 22:47:47 -0600

    Botones MenuPrincipal

---
+++ ManualAR/Assets/Scripts/MenuInicial.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuInicial : MonoBehaviour
+{
+    public void Camara()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Manuales()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+}
