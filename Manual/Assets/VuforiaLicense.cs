9a183f16e0dcf99652fbf9390d3887ee18eec2b6
PACHECO 2099 <122486806+RicardoSantana2099@users.noreply.github.com>
2024-07-02 19:29:31 -0600

    App.01

---
+++ Manual/Assets/VuforiaLicense.cs
@@ -0,0 +1,14 @@
+
+            //This class is auto-generated, please do not overwrite
+            using UnityEngine.Scripting;
+
+            public class VuforiaLicense
+            {
+const string license = "QWZEL1dwRC8vLy8vQUFBQm1Wb2IxTmtCWTBaZmhqbTNwcThjZkI4TEZWTHpSOEpTMnd6TjlqK2NFdjNZek1keHh3aE5KeEpqRFhOTUlkNHNjMnBVZlJ5VTUyL3hDaHBIUDZpandST2hOVGxobVZPTTFjVjdLK3Bwd3VpZ3VSWEZ3NkNZNUFVazZBdnJSM0t1STk5ZHpRamQySUtCbit3YU9LazVXRllwUGozY2MwczZ6SEprV21BRFZneFZRc0pLdTI0R08zUU1sQ2NZYUFsaXlkblJWQ0RKY2FnWHpLWmd0NmFnNnoyTWZsQ2kxYnhyeHR6QndSemhBaGNITmlHMkhxWDNCZ1ZrZVM3ODFObnNGMWlGMUR3bndyWkNndzMwQlVNRXgrcW91ZUpUMHNwOFRzSWxZMER3T3BXRHRCMjB0OUNQV1RJaEwraEs5VnNXcUlyL2JPYmUwQy9BSHBrTTVNL2kycXpERHp6eXRRR21EakFzTDAxT1pKR1I=";
+
+                [Preserve]
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
